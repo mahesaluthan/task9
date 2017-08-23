2fd19006c07c7ac379bee057d044a61adf5dcd88
mahesaluthan <echasmith97@gmail.com>
2017-08-23 10:22:59 +0700

    console

---
+++ ConsoleApplication1/sorting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class sorting
+    {
+        static void Main(string[] args)
+        {
+            int[] nilai = new int[10];
+            int temp;
+
+            for (int i=0; i < 10;i++ )
+            {
+                Console.Write("nilai: ");
+                nilai[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            for(int i=0;(i <= (9 -1)); i++)
+            {
+                if (nilai[i + 1] > nilai[i])
+                {
+                    temp = nilai[i];
+                    nilai[i] = nilai[i + 1];
+                    nilai[i + 1] = temp;
+
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication8/delegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    public delegate double Delegate_Prod(int a, int b);
+    class Program
+    {
+        static double fn_Provalues(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+        static void Main(string[] args)
+        {
+            Delegate_Prod exam = new Delegate_Prod(fn_Provalues);
+            Console.Write("please enter the values:  ");
+            int v1 = Int32.Parse(Console.ReadLine());
+            Console.Write("and ");
+            int v2 = Int32.Parse(Console.ReadLine());
+
+            double res = exam(v1, v2);
+            Console.WriteLine("Result :" + res);
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
